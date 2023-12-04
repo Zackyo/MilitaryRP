73a2c9cd8304312c26316b302a30b49301f7f4fc
zackyo <stianjhamborg@hotmail.com>
2023-12-04 11:56:53 +0100

    Første fungerende utkast av skriptet.

--- SHA256-Generator-Reader/sha256hashgenerator.py
+++ SHA256-Generator-Reader/sha256hashgenerator.py
@@ -1,2 +1,18 @@
 #Python script som leser innholdet i en fil og hasher innholder ved hjelp av SHA256-algoritmen.
 #Den vil opprette en ny tekstfil og lagre hash verdien i den nye filen.
+
+#Importerer nødvendig bibliotek.
+import hashlib
+
+# Åpner filen for lesing. Filen ligger på samme sted som der skriptet kjører.
+fil = open('sha256lisens.txt', 'rb')
+filinnhold = fil.read()
+fil.close()
+
+# Generer en hash av filinnholdet.
+hash = hashlib.sha256(filinnhold).hexdigest()
+
+# Oppretter en ny fil og skriver hashen til filen.
+hashfil = open('hashetsha256lisens.txt', 'w')
+hashfil.write(hash)
+hashfil.close()
